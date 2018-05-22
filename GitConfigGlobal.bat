7c73f6146fbce4cd1ff6365d85d57bfd278a1cee
Ross-RH <ross@renderheads.com>
2018-05-22 14:05:35 +0200

    test2

--- GitConfigGlobal.bat
+++ GitConfigGlobal.bat
@@ -6,7 +6,7 @@ if %%a==cacheserver set cacheserver=%%b
 
 echo setting url override for cache server
 set arg1=%1
-git config --global --replace-all url."https://%cacheserver%/".insteadOf https://
+git config --global --replace-all url."http://%cacheserver%/".insteadOf https://
 git config --global --replace-all url."https://".pushInsteadOf https://
 
 git lfs install
